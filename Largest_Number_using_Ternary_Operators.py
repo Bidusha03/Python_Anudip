537aafd86eae087b442e19c0712539fbce5b8217
Bidusha Halder <149459802+Bidusha03@users.noreply.github.com>
2023-11-01 02:59:31 -0700

    Add files via upload

---
+++ Largest_Number_using_Ternary_Operators.py
@@ -0,0 +1,8 @@
+#Declare two variables and print that which variable is largest using ternary operation
+
+num1=6
+num2=8
+largest_number = num1 if num1>num2 else num2
+print("Largest Number :",largest_number)
+
+#Output:Largest Number : 8
---
+++ area_triangle.py
@@ -0,0 +1,9 @@
+#Python Program to find the area of triangle whose sides are given.
+
+base=8
+height=6
+area=1/2*base*height
+print("The area of the triangle is:",area)
+
+
+#Output:The area of the triangle is: 24.0
