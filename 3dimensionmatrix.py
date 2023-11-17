ebddb7cbc0fe8e32b20aa005452810a7f0f37b39
Bidusha Halder <149459802+Bidusha03@users.noreply.github.com>
2023-11-17 13:29:47 +0530

    Add files via upload

---
+++ 3dimensionmatrix.py
@@ -0,0 +1,22 @@
+'''Write a NumPy program to create a 3x3 matrix with values ranging from 2 to 10.'''
+
+import numpy as np  #import numpy library ad use alias 'np'
+
+# Create a 1-dimensional array with values ranging from 2 to 10
+array = np.arange(2, 11)
+
+# Reshape the array into a 3x3 matrix
+matrix = array.reshape(3,3)
+
+# Print the matrix
+print(matrix)
+
+''' Output :
+[[ 2  3  4]
+ [ 5  6  7]
+ [ 8  9 10]]
+'''
+
+
+
+
