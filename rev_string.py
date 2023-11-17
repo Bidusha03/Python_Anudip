6172edccad63b28fd12d3767d6454347e5c090dc
Bidusha Halder <149459802+Bidusha03@users.noreply.github.com>
2023-11-17 00:06:36 -0800

    Delete rev_string.py

--- rev_string.py
+++
@@ -1,8 +0,0 @@
-"""Write a Python program to reverse words in a string
-String = “Deeptech Python Training” """
-
-str= "Deeptech Python Training"
-reverse=str[: : -1]
-print(reverse)
-
-#output:gniniarT nohtyP hcetpeeD
